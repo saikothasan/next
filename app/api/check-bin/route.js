4c8f7421d808280aa16d09193fc8abba545424c0
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-10-27 16:22:47 +0600

    Update route.js

--- app/api/check-bin/route.js
+++ app/api/check-bin/route.js
@@ -2,18 +2,24 @@ import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 import csv from 'csv-parser';
+import https from 'https';
 
 let data = [];
 
-fs.createReadStream(path.join(process.cwd(), 'data', 'bins.csv'))
-  .pipe(csv())
-  .on('data', (row) => {
-    data.push(row);
+const fetchCSV = () => {
+  https.get('https://raw.githubusercontent.com/venelinkochev/bin-list-data/refs/heads/master/bin-list-data.csv', (response) => {
+    response.pipe(csv())
+      .on('data', (row) => {
+        data.push(row);
+      });
   });
+};
+
+fetchCSV();
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const bin = searchParams.get('bin');
-  const result = data.find(item => item.bin === bin);
+  const result = data.find(item => item.BIN === bin);
   return NextResponse.json(result || { message: 'BIN not found' });
 }
